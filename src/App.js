c98cbb4d9b7c04b177aa2a0e7d0d0c99f273dbbd
Annie Huang <annie.hn@gmail.com>
2024-04-04 21:01:08 +1100

    Update jumTo to set the new currentMove value. Modify handlePlay method to take into the account of currentMove value.

--- src/App.js
+++ src/App.js
@@ -132,17 +132,26 @@ const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   // const [squares, setSquares] = useState(Array(9).fill(null));
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   const handlePlay = (nextSquares) => {
     // TODO
     // setSquares(nextSquares);
-    setHistory([...history, nextSquares]);
+    // setHistory([...history, nextSquares]);
+
+    console.log(currentMove);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+
     setXIsNext(!xIsNext);
   };
 
   const jumpTo = (nextMove) => {
     // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   };
 
   const moves = history.map((squares, move) => {
