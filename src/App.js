40c9e704946e8601fd2187f6fbd27b8ea6f78e6e
Annie Huang <annie.hn@gmail.com>
2024-04-04 20:17:04 +1100

    Create another level call <Game> to wrap around <Board>

--- src/App.js
+++ src/App.js
@@ -130,4 +130,17 @@ const Board = () => {
   );
 };
 
-export default Board;
+const Game = () => {
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
