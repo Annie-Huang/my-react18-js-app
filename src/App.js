04defa3514934851ca5d025a7da38c22dcd046d2
Annie Huang <annie.hn@gmail.com>
2024-04-04 19:26:21 +1100

    Create two square X

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <button className='square'>X</button>;
+  return (
+    <>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+    </>
+  );
 }
 
 export default App;
