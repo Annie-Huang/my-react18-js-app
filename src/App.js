06cde27e5e11b573caca8b8342c7c3d40539b470
Annie Huang <annie.hn@gmail.com>
2024-04-04 20:31:30 +1100

    Move calculateWinner function outside of Board because we don't want to rerender it again and again.

--- src/App.js
+++ src/App.js
@@ -16,34 +16,30 @@ const Square = ({ value, onSquareClick }) => {
   );
 };
 
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 // const Board = () => {
 const Board = ({ xIsNext, squares, onPlay }) => {
   /*  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));*/
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
 
   const handleClick = (i) => {
     // If it's already been set it cannot reset again. Or already got a winner, we do nothing
