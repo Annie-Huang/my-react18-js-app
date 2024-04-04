86821d05b5d83dca9931688591974e160ee6f1b8
Annie Huang <annie.hn@gmail.com>
2024-04-04 19:38:25 +1100

    Create onClick method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 const Square = ({ value }) => {
-  return <button className='square'>{value}</button>;
+  const handleClick = () => {
+    console.log('clicked!');
+  };
+
+  return (
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
 
 const Board = () => {
