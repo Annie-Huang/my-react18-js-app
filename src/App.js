39f08bb07961d93f98a13b2b355721ed7fb87579
Annie Huang <annie.hn@gmail.com>
2024-04-05 15:56:31 +1100

    Create todos array and inputValue, Create handleAddToDo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { useState } from 'react';
+
 function ToDoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = (e) => {
+    console.log('get into submit');
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue('');
+  };
+
+  console.log('inputValue', inputValue);
+
   return (
     <div className='container'>
+      {/*<form onSubmit={handleAddTodo}>*/}
       <form>
-        <input type='text' placeholder='Add a new task' />
-        <button>Add Todo</button>
+        <input
+          type='text'
+          placeholder='Add a new task'
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {/*<button type='submit'>Add Todo</button>*/}
+        <button onClick={handleAddTodo}>Add Todo</button>
       </form>
     </div>
   );
