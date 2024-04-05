6ef5765bba9b5291541d0e0d096b1fedc7e5c6ae
Annie Huang <annie.hn@gmail.com>
2024-04-05 18:09:35 +1100

    Create <PhoneBookForm> and make it as subgrid.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
 
+const PhoneBookForm = () => {
+  return (
+    <form className='form'>
+      <div className='form-row'>
+        <label htmlFor='firstName'>First name:</label>
+        <input type='text' name='firstName' id='firstName' />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='lastName'>Last name:</label>
+        <input type='text' name='lastName' id='lastName' />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='phone'>Phone:</label>
+        <input type='text' name='phone' id='phone' />
+      </div>
+    </form>
+  );
+};
 const App = () => {
   return (
     <div className='container'>
+      <PhoneBookForm />
     </div>
   );
 };
