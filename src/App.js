afa9dadcaf6d864c416f7e02b97358839dc7248e
Annie Huang <annie.hn@gmail.com>
2024-04-05 19:37:16 +1100

    Add initial value for user and reference user in input value

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
+import { useState } from 'react';
 
 const PhoneBookForm = () => {
+  const initState = {
+    id: null,
+    firstname: 'Coder',
+    lastname: 'Byte',
+    phone: '000000',
+  };
+  const [user, setUser] = useState(initState);
+
   return (
     <form className='form'>
       <div className='form-row'>
         <label htmlFor='firstName'>First name:</label>
-        <input type='text' name='firstName' id='firstName' />
+        <input
+          type='text'
+          name='firstName'
+          id='firstName'
+          value={user.firstname}
+        />
       </div>
       <div className='form-row'>
         <label htmlFor='lastName'>Last name:</label>
-        <input type='text' name='lastName' id='lastName' />
+        <input
+          type='text'
+          name='lastName'
+          id='lastName'
+          value={user.lastname}
+        />
       </div>
       <div className='form-row'>
         <label htmlFor='phone'>Phone:</label>
-        <input type='text' name='phone' id='phone' />
+        <input type='text' name='phone' id='phone' value={user.phone} />
       </div>
     </form>
   );
