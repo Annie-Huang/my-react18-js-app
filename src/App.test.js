28b514237c105fe373ef5230dd15e22d72c37cf2
Annie Huang <annie.hn@gmail.com>
2024-04-08 15:49:54 +1000

    Clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import WeatherDashboard from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<WeatherDashboard />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
