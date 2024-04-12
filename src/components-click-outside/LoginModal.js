ad68be3f5f9db06a434f4691ef971ff40d77b456
Annie Huang <annie.hn@gmail.com>
2024-04-12 17:58:16 +1000

    Focus on the first input when modal opens

--- src/components-click-outside/LoginModal.js
+++ src/components-click-outside/LoginModal.js
@@ -1,14 +1,21 @@
-import React from "react";
-import useOnClickOutside from "./useOnClickOutside";
-import "./LoginModal.css";
+import React, { useEffect, useRef } from 'react';
+import useOnClickOutside from './useOnClickOutside';
+import './LoginModal.css';
 
 function LoginModal({ onClose }) {
   const clickOutsideRef = useOnClickOutside(onClose);
+  const firstFocuableEl = useRef(null);
+
+  useEffect(() => {
+    if (firstFocuableEl) {
+      firstFocuableEl.current.focus();
+    }
+  }, []);
 
   return (
-    <div className="LoginModal-overlay-container">
+    <div className='LoginModal-overlay-container'>
       <form
-        className="LoginModal-container"
+        className='LoginModal-container'
         onSubmit={(e) => {
           e.preventDefault();
         }}
@@ -16,15 +23,15 @@ function LoginModal({ onClose }) {
       >
         <h2>Login</h2>
         <p>
-          <label className="LoginModal-label">Username: </label>
-          <input type="text" />
+          <label className='LoginModal-label'>Username: </label>
+          <input type='text' ref={firstFocuableEl} />
         </p>
         <p>
-          <label className="LoginModal-label">Password: </label>
-          <input type="password" />
+          <label className='LoginModal-label'>Password: </label>
+          <input type='password' />
         </p>
-        <button type="submit">Let me in</button>
-        <button className="LoginModal-closeButton" onClick={onClose}>
+        <button type='submit'>Let me in</button>
+        <button className='LoginModal-closeButton' onClick={onClose}>
           X
         </button>
       </form>
